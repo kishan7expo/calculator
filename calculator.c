1bde0f48c4e33cc0b0b224dc25da0d8fa92a09a1
kishan7expo <kishan.r1@s.amity.edu>
2025-07-20 21:46:35 +0530

    division

--- calculator.c
+++ calculator.c
@@ -6,12 +6,17 @@ double num1,num2,sum,difference,product,division;
 printf("enter two numbers:");
 scanf("%d%d,&num1,&num2);
 
-sum=num1+num2
+sum=num1+num2;
 printf("the sum is:%d\n",sum);
-return 0;
 
-difference=num1-num2
+difference=num1-num2;
 printf("the difference is:%d\n",difference);
 
-product=num1*num2
+product=num1*num2;
 printf("the product is:%d\n",product);
+
+division=num1/num2;
+printf("the quotient is:%d\n",division);
+
+return 0;
+}
